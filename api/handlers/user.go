6e035bef7927b2928eddc6ca5216d647ffa936fb
BRO3886 <siddverma1999@gmail.com>
2020-10-17 17:52:41 +0530

    check user login route

--- api/handlers/user.go
+++ api/handlers/user.go
@@ -8,7 +8,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func registerUser(svc user.Service) fiber.Handler {
+func register(svc user.Service) fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
 		user := &user.User{}
 
@@ -61,8 +61,48 @@ func registerUser(svc user.Service) fiber.Handler {
 	}
 }
 
+func login(svc user.Service) fiber.Handler {
+	return func(ctx *fiber.Ctx) error {
+		user := &user.User{}
+		if err := ctx.BodyParser(&user); err != nil {
+			ctx.Status(http.StatusUnprocessableEntity).JSON(fiber.Map{
+				"msg":   "unable to parse json",
+				"error": err.Error(),
+			})
+			return err
+		}
+		user, err := svc.Login(user.Username, user.Password)
+		if err != nil {
+			ctx.Status(http.StatusBadRequest).JSON(fiber.Map{
+				"msg":   "incorrect credentials",
+				"error": err.Error(),
+			})
+			return err
+		}
+		token, err := middleware.CreateToken(user.Username, user.ID)
+
+		if err != nil {
+			ctx.Status(http.StatusInternalServerError).JSON(fiber.Map{
+				"msg":   "unable to create token for user",
+				"error": err.Error(),
+			})
+			return err
+		}
+
+		user.Password = ""
+
+		ctx.Status(http.StatusOK).JSON(fiber.Map{
+			"msg":   "login successful",
+			"token": token,
+			"user":  *user,
+		})
+		return nil
+	}
+}
+
 //UserEndpoints manage user endpoints
 func UserEndpoints(app *fiber.App, svc user.Service) {
-	app.Post("/api/user/create", registerUser(svc))
+	app.Post("/api/user/create", register(svc))
+	app.Post("/api/user/login", login(svc))
 
 }
